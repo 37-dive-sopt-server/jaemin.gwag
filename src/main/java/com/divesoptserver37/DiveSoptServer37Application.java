69ea562ff15414c486fed6fd8183bd7ddba06d2f
woals2840 <woals2840@gmail.com>
2025-10-09 15:34:20 +0900

    [커밋 메시지]

---
+++ src/main/java/com/divesoptserver37/DiveSoptServer37Application.java
@@ -0,0 +1,13 @@
+package com.divesoptserver37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveSoptServer37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiveSoptServer37Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/divesoptserver37/DiveSoptServer37ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divesoptserver37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveSoptServer37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
