f76858b4e37f338f3f2e7287a95c345fc2f29389
woals2840 <woals2840@gmail.com>
2025-11-01 16:21:45 +0900

    기본 과제 구현

--- src/main/java/com/divesoptserver37/domain/dto/response/MemberInfoResponse.java
+++ src/main/java/com/divesoptserver37/domain/dto/response/MemberInfoResponse.java
@@ -4,11 +4,13 @@ import java.time.LocalDate;
 
 import com.divesoptserver37.domain.entity.Gender;
 import com.divesoptserver37.domain.entity.Member;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public record MemberInfoResponse(
 	Long id,
 	String name,
 	String email,
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
 	LocalDate birthday,
 	Gender gender
 ) {
